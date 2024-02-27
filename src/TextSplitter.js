3fb7fab2bb9980c3476cade655b9370e71443185
rs-akane <rs.akane.y@gmail.com>
2024-02-28 01:56:37 +0900

    コメント追加

--- src/TextSplitter.js
+++ src/TextSplitter.js
@@ -3,11 +3,13 @@ import React from 'react';
 
 const TextSplitter = ({text}) => {
 
+  /* 改行タグで分割し、空文字しかない行は削除しておく */
     const ary = text.split('<br/>').filter(item => item !== "");
     let newText = ary.map((item, index) => {
+      /*1行目以外各行の値と改行を結合する*/
       return index !== 0 ? (
             <React.Fragment key={index}>
-                <br/>{item}
+                {item}<br/>
             </React.Fragment>
             ):null;
           });
