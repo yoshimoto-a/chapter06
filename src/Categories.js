f7bc1e029c07db5a3701a10378d2d054d33f8ca5
rs-akane <rs.akane.y@gmail.com>
2024-02-29 09:17:30 +0900

    コメント編集

--- src/Categories.js
+++ src/Categories.js
@@ -6,7 +6,7 @@ export const Categories = ({categories}) => (
 	<div className="flex flex-wrap">
 		{
 			categories.map((category,index) => (
-				<div className="border border-solid border-blue-500 rounded text-blue-500 text-xs px-2 py-1 mr-2" key={index}>{category}</div>
+			<div className="border border-solid border-blue-500 rounded text-blue-500 text-xs px-2 py-1 mr-2" key={index}>{category}</div>
 			))
 		}
 	</div>
