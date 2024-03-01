6b6f0c6b13ba2bc13c8f004ccf5b8a804611e919
rs-akane <rs.akane.y@gmail.com>
2024-03-01 09:44:48 +0900

    if文修正

--- src/Post.js
+++ src/Post.js
@@ -6,34 +6,31 @@ import dayjs from "dayjs";
 export const Post = () => {
   const { id } = useParams();
   const targetItem = posts.find((item) => item.id == id);
+  if (!targetItem) return <div>記事が見つかりません</div>;
   return (
     <>
-      {!targetItem ? (
-        <div>記事が見つかりません</div>
-      ) : (
-        <div class="mx-auto max-w-800px">
-          <div class="flex flex-col p-4">
-            <img
-              src="https://placehold.jp/800x400.png"
-              alt=""
-              class="h-auto max-w-full"
-            ></img>
-            <div class="p-4">
-              <div class="flex justify-between">
-                <div class="text-gray-600 text-xs">
-                  {dayjs(targetItem.createdAt).format("YYYY/MM/DD")}
-                </div>
-                <Categories categories={targetItem.categories}></Categories>
+      <div class="mx-auto max-w-800px">
+        <div class="flex flex-col p-4">
+          <img
+            src="https://placehold.jp/800x400.png"
+            alt=""
+            class="h-auto max-w-full"
+          ></img>
+          <div class="p-4">
+            <div class="flex justify-between">
+              <div class="text-gray-600 text-xs">
+                {dayjs(targetItem.createdAt).format("YYYY/MM/DD")}
               </div>
-              <div class="text-lg mb-4 mt-2">{targetItem.title}</div>
-              <div
-                class="text-base leading-relaxed"
-                dangerouslySetInnerHTML={{ __html: targetItem.content }}
-              ></div>
+              <Categories categories={targetItem.categories}></Categories>
             </div>
+            <div class="text-lg mb-4 mt-2">{targetItem.title}</div>
+            <div
+              class="text-base leading-relaxed"
+              dangerouslySetInnerHTML={{ __html: targetItem.content }}
+            ></div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
